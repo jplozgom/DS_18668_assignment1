d4822915fff7800e45e9127daaba22a31ad8d35a
Juan Pablo Lozano Gómez <jplozgom@gmail.com>
2020-09-13 19:17:57 -0700

    print smells

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os, sys
+from src.smells.smells import Smell
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -9,11 +10,41 @@ import click
 def cli():
     pass
 
-# Command 1
-@cli.command()
-def initdb():
-    click.echo('Initialized the database')
+# Command 0 - List smells available
+@cli.command(name='smells')
+def listSmells():
+    """Lists all the smells available for the system and the user to work with ."""
+    click.echo("")
+    for index, listItem in enumerate(Smell):
+        click.echo(  click.style(str(index + 1) + ". " + listItem.label() + ": ", fg='green') + listItem.description() )
+    click.echo("")
 
+# Command 1 - List smells models available
+@cli.command(name='models')
+def listModels():
+    """Lists all the models available for the system and the user to work with and predict smells in data ."""
+    click.echo('Lists all the models available for the system and the user to work with and predict smells in data .')
+
+# Command 1 - Train a model for a smell
+@cli.command(name='train')
+# @click.argument('schedule_csv', type=click.Path(exists=True, readable=True))
+# @click.option('--schedule_csv', help='Filepath to schedule csv file', type=click.Path(exists=True, readable=True),required=True)
+
+@click.option('--smell', help='Smell used to train a Model.', required=True, type=click.STRING)
+@click.option('--model', help='Model used to predict a smell from a dataset.', required=True, type=click.STRING)
+def trainModel(smell, model):
+    """Trains a model for a smell."""
+    click.echo('Trains a model for a smell' + " - " + smell + " - " + model)
+
+
+# Command 2 - Predict a smell using a
+@cli.command(name='predict')
+@click.option('--smell', help='Smell used to train a Model.')
+@click.option('--model', help='Model used to predict a smell from a dataset.')
+def predictSmell():
+    """Predicts a smell using a model generated previously"""
+
+    click.echo('Predicts a smell using a model generated previously')
 
 # Command 2
 @cli.command()
---
+++ src/smells/smells.py
@@ -0,0 +1,35 @@
+from enum import Enum, unique
+
+@unique
+class Smell(Enum):
+
+    """List of available smells in the system"""
+
+    DATA_CLASS = "data class"
+    FEATURE_ENVY = "feature envy"
+    GOD_CLASS = "god class"
+    LONG_METHOD = "long method"
+
+    def description(self):
+        if(self == Smell.DATA_CLASS):
+            # https://refactoring.guru/smells/data-class
+            return "A data class refers to a class that contains only fields and crude methods for accessing them (getters and setters)."
+        if(self == Smell.FEATURE_ENVY):
+            # https://refactoring.guru/smells/feature-envy
+            return "A method accesses the data of another object more than its own data."
+        if(self == Smell.GOD_CLASS):
+            return "A class that knows too much or does too much. Also known as large class"
+        if(self == Smell.LONG_METHOD):
+            return "A method contains too many lines of code. Usually more than 10"
+
+    def label(self):
+        """ The label used for each smell in a list """
+        return str(self.value).upper()
+
+    def getDatasetFileName(self):
+        """ returns the filename of the dataset that belongs to a smell """
+        return str(self.value).replace(" ", "-") + ".arff"
+
+    @staticmethod
+    def getList():
+        return list(map(str, Smell))
